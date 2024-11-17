b678688d33e411f5aec63528e121752fb3ee2985
ASDFGH-ALEX <alexsabu053@gmail.com>
2024-11-17 21:36:01 +0530

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,9 @@
+'''
+Author:Alex
+Date=17-10-2024
+Description:Write a program to add two numbers.
+'''
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+add=num1+num2
+print(add)
---
+++ maximum.py
@@ -0,0 +1,9 @@
+'''
+Author:Alex
+Date=17-10-2024
+Description:Write a program to find the maximum of two numbers
+'''
+a=2
+b=5
+Maximum=max(a,b)
+print(Maximum)
